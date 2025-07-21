5aacac0c838350afb6b21c25762446f965bc1441
Alibek <alibek@MacBook-Pro-Alibek.local>
2025-07-21 22:00:29 +0500

    fix: disabled directory listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 	"log"
+	"strings"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/simp-target/view", simpTargetView)
@@ -19,4 +20,15 @@ func main() {
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func neuter(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        if strings.HasSuffix(r.URL.Path, "/") {
+            http.NotFound(w, r)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
 }
