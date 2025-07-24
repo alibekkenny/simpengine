5804dcae4b9fc0d250a6510092b83d0335f583cb
Alibek <alibek@MacBook-Pro-Alibek.local>
2025-07-24 13:07:53 +0500

    feat: added custom loggers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
 	"fmt"
 	"html/template"
-	"log"
+	"net/http"
+	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,29 +21,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500) 
+		app.errorLog.Print(err.Error()) // old default logger
+		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500) 
+		app.errorLog.Print(err.Error()) // old default logger
+		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func simpTargetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) simpTargetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 0 {
 		http.NotFound(w, r)
 		return
 	}
-	
+
 	w.Write([]byte(fmt.Sprintf("View simp target with id: %v", id)))
 }
 
-func simpTargetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) simpTargetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,34 +1,59 @@
 package main
 
 import (
-	"net/http"
+	"flag"
 	"log"
+	"net/http"
+	"os"
 	"strings"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	// Initialize a new instance of our application struct, containing the dependencies.
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	// Swap the route declarations to use the application struct's methods as the handler functions.
 	mux := http.NewServeMux()
-	
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/simp-target/view", simpTargetView)
-	mux.HandleFunc("/simp-target/create", simpTargetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/simp-target/view", app.simpTargetView)
+	mux.HandleFunc("/simp-target/create", app.simpTargetCreate)
 
-	
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func neuter(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if strings.HasSuffix(r.URL.Path, "/") {
-            http.NotFound(w, r)
-            return
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
