6207f45b6062405a29f770770ac78fa61a201cac
Danial Muntinov <danialmuntinov@MacBook-Air-Danial.local>
2025-09-19 00:13:24 +0500

    fix(middleware): changed cors policy

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -24,6 +24,12 @@ func (app *application) secureHeaders(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", app.config.FrontEndHost)
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 		}
 		next.ServeHTTP(w, r)
 	})
